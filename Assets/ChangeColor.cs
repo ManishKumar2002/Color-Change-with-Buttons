9ffee295a1dcb4cb1cd50c12a9fecf4d5464a0b4
Manish Kumar <manish10akbr@gmail.com>
2022-12-08 03:04:08 +0530

    project files

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Renderer SphereRenderer;
+    public GameObject Sphere;
+    [SerializeField] private Color newColor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SphereRenderer = Sphere.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeMaterial()
+    {
+        SphereRenderer.material.color = newColor;
+        
+    }
+}
